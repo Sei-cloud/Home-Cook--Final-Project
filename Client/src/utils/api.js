350b24d43de7596e7501e95c344ecc4eda53a3ad
Sei Munkhbat <sei@Seis-MacBook-Pro.local>
2024-07-30 18:37:18 -0500

    Working api, Added some styles

---
+++ Client/src/utils/api.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php';
+
+export const searchRecipes = async (query) => {
+  try {
+    const response = await fetch(`${API_URL}?s=${query}`);
+    const data = await response.json();
+    return data.meals || [];
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+    return [];
+  }
+};
