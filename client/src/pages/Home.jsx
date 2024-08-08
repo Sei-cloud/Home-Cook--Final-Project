65cad3ff0469dd6a75c145e9b2a3523ddc06b716
Sei Munkhbat <sei@Seis-MacBook-Pro.local>
2024-08-08 17:11:05 -0500

    Added a footer with github links

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import RecipeSearch from '../components/RecipeSearch';
-import SignupForm from './Register';
-import RecipeList from '../components/RecipeList';
-import '../styles/styles.css';
-import Auth from '../utils/auth'; 
+import React, { useState } from "react";
+import RecipeSearch from "../components/RecipeSearch";
+import SignupForm from "./Register";
+import RecipeList from "../components/RecipeList";
+import "../styles/styles.css";
+import Auth from "../utils/auth";
 
 const Home = () => {
-   // State to store the list of recipes
   const [recipes, setRecipes] = useState([]);
-  // Handler to update the recipes state
+
   const recipeHandler = (array) => {
     setRecipes(array);
   };
 
   return (
-    <div className="App home-page container"> {/* Apply both App and home-page classes here */}
+    <div className="App home-page container">
       <div className="hero-banner">
         <div className="hero-content">
           <h1>Welcome to Home-Cook</h1>
-          <p>Discover and save your favorite recipes, or share your own culinary creations!</p>
+          <p>
+            Discover and save your favorite recipes, or share your own culinary
+            creations!
+          </p>
           {!Auth.loggedIn() && ( // Conditionally render the button
-            <button className="hero-button" onClick={() => window.location.href = '/register'}>
+            <button
+              className="hero-button"
+              onClick={() => (window.location.href = "/register")}
+            >
               Sign Up Now
             </button>
           )}
@@ -31,13 +36,39 @@ const Home = () => {
         <RecipeSearch recipeHandler={recipeHandler} />
         {recipes.length > 0 && <RecipeList recipes={recipes} />}
       </div>
+      <footer className="footer">
+        <div className="github-links">
+          <span>Mady by</span>
+          <a
+            href="https://github.com/Sam-Cowman"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Samantha
+          </a>
+          <a
+            href="https://github.com/charliejb1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Charlie
+          </a>
+          <a
+            href="https://github.com/Sei-cloud"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sei
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
 
 const Signup = () => {
   return (
-    <div className="App"> 
+    <div className="App">
       <h1>Home-Cook</h1>
       <SignupForm />
     </div>
